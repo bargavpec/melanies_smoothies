f08e883067f8427a311c3b0c1efe61f631aae9ab
bargavpec <sai.honey31@gmail.com>
2024-10-27 15:22:41 +0530

    Update streamlit_app.py
    Adding requests package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -35,3 +35,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    st.text(fruityvice_response)
+
