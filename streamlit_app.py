9f582e9ceb48a1a1138fa09aefb1736894697b9a
bargavpec <sai.honey31@gmail.com>
2024-10-27 15:48:36 +0530

    Update streamlit_app.py
    adding loc

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list=st.multiselect('Select 5 ingredients',my_dataframe,max_selections=6)
 if ingredients_list:
@@ -30,6 +30,8 @@ if ingredients_list:
     ingredients_string=''
     for x in ingredients_list:
         ingredients_string+=x+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader('Nutrition information of' +x)
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         st.dataframe(data=fruityvice_response.json(), use_container_width=True)
