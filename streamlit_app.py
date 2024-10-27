6a1218ea809858d7b0a230a7dee53e3daab5b357
bargavpec <sai.honey31@gmail.com>
2024-10-27 15:28:49 +0530

    Update streamlit_app.py
    dataframe changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response.json())
+st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
